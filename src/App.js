75a055cef1922a807b52eead73134b6d78932dab
Dave Timmins <davetimmins@outlook.com>
2017-04-07 09:14:32 +1200

    use esri-loader-react

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { isLoaded, bootstrap, dojoRequire } from 'esri-loader';
+import { dojoRequire } from 'esri-loader';
+import EsriLoader from 'esri-loader-react';
 
-class App extends Component {
+class App extends React.PureComponent {
 
-  createMap() {
+  createMap = () => {
 
     dojoRequire(['esri/Map', 'esri/views/MapView'], (Map, MapView) => { 
       new MapView({
-        container: this.refs.mapView,
+        container: this.mapContainer,
         map: new Map({basemap: 'topo'})
       })
     });
   }
 
-  componentWillMount() {
-    
-    if (!isLoaded()) {
-      bootstrap((err) => {
-        if (err) {
-          console.error(err);
-        }
-
-        this.createMap();
-      }, {
-        url: 'https://js.arcgis.com/4.1/'
-      });
-    } else {
-      
-      this.createMap();
-    }
-  }
-
   render() {
+
+    const options = {
+      url: 'https://js.arcgis.com/4.3/'
+    };
 
     return (
       <div className="App">
+        <EsriLoader options={options} ready={this.createMap} />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -44,7 +32,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <div ref='mapView' className='map-view'></div>
+        <div ref={node => this.mapContainer = node} className='map-view'></div>
       </div>
     );
   }
