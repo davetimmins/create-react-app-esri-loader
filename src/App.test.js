b68ad4aedd81e4e0b957be23daacd1ce8518fd68
Dave Timmins <davetimmins@outlook.com>
2017-07-13 10:59:44 +1200

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+  const component = shallow(
+    <App />
+  );
+
+  expect(component.text()).toContain('Welcome to React');
 });
