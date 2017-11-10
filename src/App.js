e22f69c7fab637381eb011f1dcb7d2247f12b045
Dave Timmins <davetimmins@outlook.com>
2017-11-11 12:53:41 +1300

    update dependencies

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { dojoRequire } from 'esri-loader';
-import EsriLoader from 'esri-loader-react';
+import EsriLoaderReact from 'esri-loader-react';
 
 class App extends React.PureComponent {
-
-  createMap = () => {
-
-    dojoRequire(['esri/Map', 'esri/views/MapView'], (Map, MapView) => { 
-      new MapView({
-        container: this.mapContainer,
-        map: new Map({basemap: 'topo'})
-      })
-    });
-  }
 
   render() {
 
     const options = {
-      url: 'https://js.arcgis.com/4.4/'
+      url: 'https://js.arcgis.com/4.5/'
     };
 
     return (
       <div className="App">
-        <EsriLoader options={options} ready={this.createMap} />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -32,7 +20,17 @@ class App extends React.PureComponent {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <div ref={node => this.mapContainer = node} className='map-view'></div>
+        <EsriLoaderReact 
+          options={options} 
+          modulesToLoad={['esri/Map', 'esri/views/MapView']}    
+          onReady={({loadedModules: [Map, MapView], containerNode}) => {
+            new MapView({
+              container: containerNode,
+              map: new Map({basemap: 'topo'})
+            })
+          }}
+          onError={error => console.error(error)}
+        />
       </div>
     );
   }
